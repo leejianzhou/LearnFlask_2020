9a7e544a71bc2e23e59b2bafd8dd9991cd8b84b6
leejianzhou@gmail.com <Sunky.86131>
2020-02-18 23:07:03 +0800

    20200218 1836

--- hello.py
+++ hello.py
@@ -9,7 +9,7 @@ def index():
 
 
 @app.route('/hi')
-@app.route('/hello')
+@app.route('/hello', methods=['GET', 'POST'])
 def say_hello():
     return render_template('hi_request.html')
 
@@ -24,6 +24,10 @@ def getrequest():
     ##以http://127.0.0.1:8080/getrequest?name=Grey
     name=request.args.get('name','Flask')
     return '<h1>Hello, %s!<h1>' % name
+
+@app.route('/goback/<int:year>')
+def go_back(year):
+    return '<p> 出生年：%d </p>' % (2020-year)
 
 if __name__ == '__main__':
     app.run()
